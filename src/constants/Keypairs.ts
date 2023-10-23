7b388d7c09975e7d272b2d351ad238baf7270782
kemmanuel662@gmail.com <EmmanuelKD>
2023-10-23 14:21:34 +0000

    nuildable

--- src/constants/Keypairs.ts
+++ src/constants/Keypairs.ts
@@ -4,5 +4,5 @@ import { Keypair } from "@solana/web3.js";
 // is used on mainnet.
 export const ANTI_BOT_DEV_AUTHORITY_KEYPAIR = Keypair.fromSecretKey(
   // REPLACEME
-  Uint8Array.from([0])
+  Uint8Array.from([206,137,58,190,15,168,150,209,44,16,230,192,95,189,137,106,234,91,102,44,134,118,28,148,142,148,135,144,242,152,122,87,134,152,98,80,93,88,87,84,80,239,177,205,150,5,24,74,193,252,119,52,32,76,203,53,49,38,201,75,236,15,95,20])
 );
